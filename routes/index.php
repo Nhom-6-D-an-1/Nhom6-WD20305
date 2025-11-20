39de555ce77ba027079c8921623cd9b45e40b019
tupv <pvtu206@gmail.com>
2025-11-20 19:02:02 +0700

    Lần đầu đẩy code

--- routes/index.php
+++ routes/index.php
@@ -4,4 +4,10 @@ $action = $_GET['action'] ?? '/';
 
 match ($action) {
     '/'         => (new HomeController)->index(),
+    'viewschedule'         => (new HomeController)->viewSchedule(),
+    'viewcustomers'         => (new HomeController)->viewCustomers(),
+    'viewdiary'         => (new HomeController)->viewDiary(),
+    'viewcheck-in'         => (new HomeController)->viewCheckin(),
+    'viewrequest'         => (new HomeController)->viewRequest(),
+    'viewreport'         => (new HomeController)->viewReport(),
 };
