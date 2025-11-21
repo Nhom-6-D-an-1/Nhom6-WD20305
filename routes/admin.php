9618e7d2d47bac61f662de8504fbac8697b2d26b
trinhht <htrinh2k3@gmail.com>
2025-11-21 08:46:03 +0700

    Sửa router phân quyền

---
+++ routes/admin.php
@@ -0,0 +1,11 @@
+<?php
+$action = $_GET['action'] ?? '/';
+if($_SESSION['user']&& $_SERVER['user']['role']== 'admin'){
+    match($action){
+        '/' =>(new AdminController)->Home(),
+    }
+}else{
+    $_SESSION['flash_error']="Bạn không có quyền truy cập trang này!";
+    header("Location: " . BASE_URL);
+    exit();
+}
